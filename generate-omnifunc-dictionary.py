c65395074166aaf48b77be81ab0b3844d29e9750
Doug Richardson <doug@rekt.email>
2019-06-02 19:07:41 -0700

    first pass omnifunc dictionary generator

---
+++ generate-omnifunc-dictionary.py
@@ -0,0 +1,91 @@
+import json
+import subprocess
+
+# vcc gives us 2 things: a list of VEX context names and and a way to dump the
+# VEX functions and global variables from each VEX context.
+# We use these 2 things to build a single uber context that is the union of all
+# other contexts. This uber context is used for completion since Vim, running
+# as an external editor from Houdini, has no idea which specific context it is in.
+#
+# vcc must be in your PATH. One way to do this is to run this python script
+# from the Houdini Command Line Tools.
+#
+# For more information on vcc, see:
+# https://www.sidefx.com/docs/houdini/vex/vcc.html
+#
+#
+# Example Output from vcc:
+#
+# $ vcc --list-context-json
+# [
+#        "surface",
+#        "displace",
+#        "light",
+#        ...
+#        ]
+#
+
+#
+# $ vcc --list-context-json sop
+#
+# {
+#  "context": "Sop",
+#  "globals": {
+#            "P": {
+#                "type": "vector",
+#                "read": true,
+#                "write": true
+#                },
+#            ...},
+#  "functions": {
+#            "sin": [
+#                {
+#                    "args": [ "const float"],
+#                    "return": "float"
+#                    },
+#                {
+#                    "args": [ "const vector4"],
+#                    "return": "vector4"
+#                    },
+#                {
+#                    "args": [ "const vector2"],
+#                    "return": "vector2"
+#                    },
+#                {
+#                    "args": [ "const vector"],
+#                    "return": "vector"
+#                    }
+#                ],
+#            ...
+#            },
+#
+
+# contextNames = json.loads(subprocess.check_output(["vcc", "--list-context-json"]))
+contextNames = ["sop"]
+ 
+# Build a dictionary of keys that match the completion. The value for each
+# key is an array of items that match it (the variations).
+# The VEX context should be part of it.
+
+# Generate a dictionary that makes it easy to implement the omnifunc completer.
+# call add(res, { 'word': m, 'menu': 'void(int[] points, vector direction)', 'kind': 'f' })
+# let sin = [{'word': 'sin', 'kind': 'f', 'menu': 'float sin(float angle)', 'dup': 1}, {'word': 'sin', 'kind': 'f', 'menu': 'double sin(double angle)', 'dup': 1, }]
+
+print("fun! vex#omnifuncdata#Load()")  
+print("  let contexts = {}")
+for contextName in contextNames:
+    context = json.loads(subprocess.check_output(["vcc", "--list-context-json", contextName]))
+    print("\n  # %s context" % (contextName))
+    print("  let c = {}")
+    for key, value in context["globals"].items():
+        print("  let c['%s'] = [{'word': '%s', 'kind': 'v', 'menu': '%s'}]" %(key, key, value["type"]))
+    for key, value, in context["functions"].items():
+        print("  let f = []")
+        for variation in value:
+            funcDecl = variation["return"] + "(" + ",".join(variation["args"]) + ")"
+            print("  add(f, {'word': '%s', 'kind': 'f', 'menu': '%s'}" %(key, funcDecl))
+        print("  let c['%s'] = f" % (key))
+    print("  let context['%s'] = context" % (contextName))
+print("\n  return contexts")
+print("endfun")
+
